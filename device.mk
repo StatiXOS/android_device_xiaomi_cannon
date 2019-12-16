bb4ad6b05df6a88756dab5c5cdb12202b03b6a3d
Bowgo Tsai <bowgotsai@google.com>
2019-12-16 11:51:20 +0800

    cannong: Adding developer GSI keys
    Bug: 144399552
    Test: tree-hugger
    Change-Id: I5012758b4f25bd0d14bca906358ecee0fa366a7b
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- device.mk
+++ device.mk
@@ -8,6 +8,9 @@
 # Build specifications
 #
 
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
+
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
