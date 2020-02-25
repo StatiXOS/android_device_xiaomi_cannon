6cba6953fe289e4ab9af42b64a8204410ccca458
Kshitij Gupta <kshitijgm@gmail.com>
2020-02-25 20:49:07 +0000

    cannong: Use a custom Lights HAL
    - Based on Lights from violet
    - See: https://github.com/PotatoDevices/device_xiaomi_violet/tree/croquette-release/light
    
    Change-Id: I25bb063db135281d53df081ebb77a535b4ade593
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- cannon.mk
+++ cannon.mk
@@ -275,6 +275,10 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.0-impl \
     android.hardware.keymaster@4.0-service
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-service.cannon
+
 # Media
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
