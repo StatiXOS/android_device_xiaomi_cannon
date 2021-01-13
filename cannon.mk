9e8f35a61fc9fafddeeb5385f56ac00063163d71
xandroid64 <xandroid64@users.noreply.github.com>
2021-01-13 20:47:49 +0530

    cannong: Use 4GB Dalvik heap configuration
    * this is a 4GB device.
    
    Change-Id: Ie26b8e4fefea25fa818bd1eaf9df4a0bb153d42d
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- cannon.mk
+++ cannon.mk
@@ -10,6 +10,15 @@ $(call inherit-product-if-exists, vendor/xiaomi/cannon/cannon-vendor.mk)
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# Setup Dalvik VM configurations
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=16m \
+    dalvik.vm.heapgrowthlimit=256m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=4m \
+    dalvik.vm.heapmaxfree=8m
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
