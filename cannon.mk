a84c43c276cca528426860fc1b3a46459768861c
Vaisakh Murali <vaisakhmurali@gmail.com>
2021-02-17 13:05:17 +0530

    cannong: Build thermal HIDL hals
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- cannon.mk
+++ cannon.mk
@@ -164,4 +164,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/product-permissions-mediatek.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/product-permissions-mediatek.xml
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service \
+    android.hardware.thermal@2.0-impl \
+    android.hardware.thermal@2.0-service
+
 $(call inherit-product-if-exists, vendor/xiaomi/cannon/device-vendor.mk)
