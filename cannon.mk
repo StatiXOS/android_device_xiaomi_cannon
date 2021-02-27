191329778976a5115a4887d9647b0421672ab070
Vaisakh Murali <vaisakhmurali@gmail.com>
2021-02-27 16:47:51 +0530

    cannong: Downgrade display and graphics stack to A10
    * The display blobs depend on the kernel driver version, making it hard
      for us to use latest blobs without the latest kernel.
    * Downgrade the display stack to A10, with patched dependant blobs.
    * Ship two variants of libion_mtk and libdpframework to satisfy A10
      dependency of display blobs, and A11 dependency of other blobs.
    
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- cannon.mk
+++ cannon.mk
@@ -148,12 +148,13 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.allocator@4.0-impl \
-    android.hardware.graphics.allocator@4.0-service \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.composer@2.1-service \
-    android.hardware.graphics.mapper@4.0-impl \
-    android.hardware.memtrack@1.0-impl
+    android.hardware.graphics.mapper@2.0-impl-2.1 \
+    android.hardware.memtrack@1.0-impl \
+    android.harwdare.memtrack@1.0-service
 
 PRODUCT_PACKAGES += \
     libdrm
