b4a0fd36ab9480eeffa91910196d1cba6fe80f92
Vaisakh Murali <vaisakhmurali@gmail.com>
2021-02-28 14:41:38 +0530

    cannong: Build camera 3.6 device hal
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- cannon.mk
+++ cannon.mk
@@ -131,6 +131,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.device@3.5 \
+    android.hardware.camera.device@3.6 \
     android.hardware.camera.provider@2.4 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
