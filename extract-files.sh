e10e91a7da6f4b4e93e8d912d82fb1a713db065d
Vaisakh Murali <vaisakhmurali@gmail.com>
2021-03-09 13:30:29 +0530

    cannong: make libaedv depend on prebuilt unwindstack
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -62,7 +62,7 @@ function blob_fixup() {
         vendor/lib/libaedv.so)
             ;&
         vendor/lib64/libaedv.so)
-            "${PATCHELF}" --add-needed "libunwindstack-v29.so" "${2}"
+            "${PATCHELF}" --replace-needed "libunwindstack.so" "libunwindstack-v29.so" "${2}"
             ;;
     esac
 }
