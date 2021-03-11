f360cbc84ee95f70c133d6b30b5aa6880a00b0de
Kshitij Gupta <kshitijgm@gmail.com>
2021-03-11 11:46:24 +0530

    cannong: Build android.hardware.audio.common@6.0-util
    - Fixes audio crash during boot
    
    Change-Id: Ibf6efde38ecd12938f415591d65e7e2f1e0c306e
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- cannon.mk
+++ cannon.mk
@@ -120,6 +120,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@5.0 \
     android.hardware.audio@5.0-impl \
+    android.hardware.audio.common@6.0-util \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.soundtrigger@2.0-core \
     android.hardware.soundtrigger@2.2 \
