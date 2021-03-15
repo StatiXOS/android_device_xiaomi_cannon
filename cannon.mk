fa05a95c9b03ad0d12306c78f4767f40d6c9580c
Vaisakh Murali <vaisakhmurali@gmail.com>
2021-03-15 16:22:29 +0530

    cannong: Match stock dalvik VM configuration
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- cannon.mk
+++ cannon.mk
@@ -173,11 +173,10 @@ PRODUCT_PACKAGES += \
 
 # Setup Dalvik VM configurations
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=16m \
     dalvik.vm.heapgrowthlimit=256m \
     dalvik.vm.heapsize=512m \
     dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=4m \
+    dalvik.vm.heapminfree=512k \
     dalvik.vm.heapmaxfree=8m
 
 # Fingerprint
