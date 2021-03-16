10a86e39d3742a338e4f3ddf95a44297a30b1f18
Vaisakh Murali <vaisakhmurali@gmail.com>
2021-03-16 19:03:25 +0530

    cannong: Drop camera.provider@2.4 hal build
    * We use prebuilt camera HAL
    * Keeps spamming the logs for missing interface
    
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- cannon.mk
+++ cannon.mk
@@ -139,7 +139,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.device@3.6 \
     android.hardware.camera.provider@2.4 \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5 \
     android.hardware.camera.provider@2.6
 
