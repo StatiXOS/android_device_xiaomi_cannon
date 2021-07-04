0f5e4bb0d50b36174d677617db6f2be5f80021b1
Vaisakh Murali <mvaisakh@statixos.com>
2021-07-04 08:55:54 +0530

    cannong: Initial system properties
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -84,6 +84,9 @@ TARGET_USERIMAGES_USE_F2FS := true
 TARGET_COPY_OUT_VENDOR := vendor
 TARGET_USES_MKE2FS := true
 
+# Props
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.mt6853
 
