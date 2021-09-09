cab814123bd5e78a208e4776873fa57f1ab4cc6c
Vaisakh Murali <mvaisakh@statixos.com>
2021-09-09 21:38:08 +0530

    cannong: Copy over stock seccomp policy configuration
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- device.mk
+++ device.mk
@@ -271,6 +271,12 @@ PRODUCT_PACKAGES += \
     ramdisk-fstab.mt6853 \
     ueventd.mtk.rc
 
+# Seccomp Policy
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/configs/seccomp/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy \
+    $(LOCAL_PATH)/configs/seccomp/mediaswcodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaswcodec.policy
+
 # Secure Element
 PRODUCT_PACKAGES += \
     android.hardware.secure_element@1.0
