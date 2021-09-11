b763e8f368502fabf1a06de2e90cebfa2702f48d
Vaisakh Murali <mvaisakh@statixos.com>
2021-09-12 00:20:11 +0530

    cannong: Build android.hardware.camera.device@3.6 interface
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- device.mk
+++ device.mk
@@ -117,7 +117,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4 \
     android.hardware.camera.provider@2.5 \
     android.hardware.camera.provider@2.6 \
-    android.hardware.camera.device@3.5
+    android.hardware.camera.device@3.5 \
+    android.hardware.camera.device@3.6
 
 # Configstore
 PRODUCT_PACKAGES += \
