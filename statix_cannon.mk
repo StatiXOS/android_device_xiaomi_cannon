9f15e2efe99c21d7c9a02847b7c1af4355340d12
Vaisakh Murali <mvaisakh@statixos.com>
2021-09-13 14:27:27 +0530

    cannong: Enable GApps by default for release builds
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- statix_cannon.mk
+++ statix_cannon.mk
@@ -8,9 +8,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-# Disable GApps for bringup
-TARGET_DOES_NOT_USE_GAPPS := true
-
 # Inherit statix common configuration
 $(call inherit-product, vendor/statix/config/common.mk)
 $(call inherit-product, vendor/statix/config/gsm.mk)
