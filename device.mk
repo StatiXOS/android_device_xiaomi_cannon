311201fc99f5880fc35aa5701d9baaab768459a1
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2021-11-14 17:14:25 +0600

    cannong: Define extra VNDK version
    Since we use stock VNDK 30 blobs, there is a dependency on older VNDK
    blobs. Defining this flag allows copying over older VNDK 30 blobs.
    
    Change-Id: I9b75f31692122af5b952b382ba6ff544ffd94178
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Reviewed-on: https://review.statixos.com/c/android_device_xiaomi_cannon/+/6241
    Reviewed-by: Vaisakh Murali <vaisakhmurali@gmail.com>
    Tested-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- device.mk
+++ device.mk
@@ -366,6 +366,8 @@ PRODUCT_COPY_FILES += \
     prebuilts/vndk/v30/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v30.so \
     prebuilts/vndk/v30/arm64/arch-arm64-armv8-a/shared/vndk-sp/libunwindstack.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libunwindstack-v30.so
 
+PRODUCT_EXTRA_VNDK_VERSIONS := 30
+
 PRODUCT_PACKAGES += \
     vndservicemanager
 
