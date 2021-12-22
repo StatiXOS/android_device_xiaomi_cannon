ac5d0d52a6f00999ecd13df7357ab63e922333d2
Vaisakh Murali <mvaisakh@statixos.com>
2021-12-22 14:03:28 -0500

    cannong: Enforce selinux
    Drop the flag entirely
    
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Change-Id: Ice0f6fd90a7f41ac3a0e4cad4bdfe7f538a55da7
    Reviewed-on: https://review.statixos.com/c/android_device_xiaomi_cannon/+/6510
    Reviewed-by: Vaisakh Murali <vaisakhmurali@gmail.com>
    Tested-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,7 +43,6 @@ TARGET_USES_UEFI := true
 # Kernel
 BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.usbconfigfs=true
-#BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_TAGS_OFFSET := 0x0bc08000
