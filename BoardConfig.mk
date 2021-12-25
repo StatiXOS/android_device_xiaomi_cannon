1ee7c7ac2d777955ad4d6442f7bca8fbf8aaa31c
Vaisakh Murali <mvaisakh@statixos.com>
2021-12-25 06:16:26 -0500

    cannong: sepolicy: Resolve platform_app denials
    Rseolves screenshot not able to capture
    Needs https://review.statixos.com/c/android_system_sepolicy/+/6531
    
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Change-Id: I246df5d924a7d5735ed001d66726a02aca655ff6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -159,6 +159,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # Sepolicy
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
 # Treble
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
