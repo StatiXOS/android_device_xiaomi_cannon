86ffa96b865238ff4c9ffe9ca1a950e62b1cd2f0
LinkBoi00 <50962670+LinkBoi00@users.noreply.github.com>
2022-01-02 19:58:38 +0200

    cannong: rootdir: Re-organize DT2W rootdir
    Signed-off-by: LinkBoi00 <50962670+LinkBoi00@users.noreply.github.com>
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Change-Id: I45597df83759de5a6a6d7856ba5745d32353e97b

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -266,3 +266,11 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/ueventd.mtk.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.dt2w.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.dt2w.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
