653bd3b24268045672cff2c7b0e34cc5af91b952
Vaisakh Murali <mvaisakh@statixos.com>
2022-04-17 21:58:51 +0600

    cannong: Extensions: Refactor double tap service
    Change-Id: I6d5a81b29638960448151b8e7bbe7a4dd9deddbf
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- CannonExtensions/src/com/android/cannon/extensions/OnBootCompleteReceiver.kt
+++ CannonExtensions/src/com/android/cannon/extensions/OnBootCompleteReceiver.kt
@@ -1,12 +1,22 @@
+/*
+ * Copyright (C) 2022 StatiX
+ * SPDX-License-Identifier: GPL-2.0-only
+ */
+
 package com.android.cannonextensions
 
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 
-class OnBootCompleteReceiver : BroadcastReceiver() {
-    override fun onReceive(context: Context, intent: Intent?) {
-        val sIntent = Intent(context, DT2WServiceCannon::class.java)
-        context.startServiceAsUser(sIntent, UserHandle.CURRENT)
+class BootCompletedReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        Log.d(TAG, "Starting")
+        DoubleTapUtils.startService(context)
+    }
+
+    companion object {
+        private const val TAG = "CannonExtensions"
     }
 }
