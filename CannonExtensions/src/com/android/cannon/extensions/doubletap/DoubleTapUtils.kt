255e3c26196178a8f55a2bfaaf6ca4b43b450fbe
Vaisakh Murali <mvaisakh@statixos.com>
2022-04-17 21:58:51 +0600

    cannong: Extensions: Refactor double tap service
    Change-Id: I6d5a81b29638960448151b8e7bbe7a4dd9deddbf
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

---
+++ CannonExtensions/src/com/android/cannon/extensions/doubletap/DoubleTapUtils.kt
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2022 StatiX
+ * SPDX-License-Identifier: GPL-2.0-only
+ */
+
+package com.android.cannonextensions
+
+import android.content.Context
+import android.content.Intent
+import android.os.UserHandle
+import android.util.Log
+
+object DoubleTapUtils {
+    private const val TAG = "DT2WServiceCannon"
+
+    fun startService(context: Context) {
+        Log.d(TAG, "Starting service")
+        context.startServiceAsUser(Intent(context, DoubleTapService::class.java), UserHandle.CURRENT)
+    }
+
+    fun stopService(context: Context) {
+        Log.d(TAG, "Stopping service")
+        context.stopServiceAsUser(Intent(context, DoubleTapService::class.java), UserHandle.CURRENT)
+    }
+}
