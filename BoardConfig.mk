ad465e0f9d101ce5f750ed2f8bcd221b9ba7e5a1
Vaisakh Murali <mvaisakh@statixos.com>
2022-04-21 22:21:58 +0600

    cannong: Drop dummy hidl libs
    Change-Id: If3006113e2ba900f007323e101dac0c6e83eccbc
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -133,6 +133,8 @@ TARGET_SCREEN_DENSITY := 440
 # HIDL VINTF Manifsets
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/manifest.xml
 DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/configs/compatibility_matrix.xml
+ODM_MANIFEST_SKUS := \
+
 ODM_MANIFEST_FILES   := \
 	$(DEVICE_PATH)/configs/manifest_dsds.xml \
 	$(DEVICE_PATH)/configs/manifest_qsqs.xml \
