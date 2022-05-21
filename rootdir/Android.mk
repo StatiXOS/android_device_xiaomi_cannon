AOSP-on-Cannon/device_xiaomi_cannon

cannong: Add init script for usb detection in recovery
Use default usb rc and stem

Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
Change-Id: I71ee5ff4643e475b24520d98329b71dafc3136d0

rootdir/Android.mk
@@ -194,6 +194,15 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.recovery.mt6853.usb.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.mt6853.usb.rc
+LOCAL_MODULE_STEM  := init.mt6853.usb.rc
+LOCAL_MODULE_PATH  := $(TARGET_RAMDISK_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.project.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC

StatiXOS/android_device_xiaomi_cannon

rootdir/Android.mk
@@ -89,6 +89,15 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.recovery.mt6853.usb.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.mt6853.usb.rc
+LOCAL_MODULE_STEM  := init.mt6853.usb.rc
+LOCAL_MODULE_PATH  := $(TARGET_RECOVERY_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.project.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
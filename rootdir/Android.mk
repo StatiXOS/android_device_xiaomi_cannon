63bce43a960263c832e9d02b20c47284e912d183
Vaisakh Murali <mvaisakh@statixos.com>
2022-03-17 21:56:42 +0600

    cannong: Add init script for usb detection in recovery
    Use default usb rc and stem
    
    Change-Id: I71ee5ff4643e475b24520d98329b71dafc3136d0
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -89,6 +89,15 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.recovery.mt6853.usb.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.mt6853.usb.rc
+LOCAL_MODULE_STEM  := init.mt6853.usb.rc
+LOCAL_MODULE_PATH  := $(TARGET_RECOVERY_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.project.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
