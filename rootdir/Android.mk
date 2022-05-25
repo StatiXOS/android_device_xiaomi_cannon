a75a39183b9726e9b4ca7932122a4341ef456a76
Marc Bourgoin <themard85@gmail.com>
2022-05-25 06:43:41 +0000

    cannong: Symlink bootdevice for Lineage Recovery
    * It is needed by the OpenGApps install script.
    
    Change-Id: Idb90a6ecaae541e74a9878c1ca93459cc2fdd949
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Reviewed-on: https://review.statixos.com/c/android_device_xiaomi_cannon/+/7805

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -89,6 +89,14 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.recovery.mt6853.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.recovery.mt6853.rc
+LOCAL_MODULE_PATH  := $(TARGET_RECOVERY_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.project.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
