0c95861c7aec27b3ee26758d5cbbee0263df9b19
Vaisakh Murali <mvaisakh@statixos.com>
2022-07-13 14:48:14 +0530

    cannong: Upgrade blobs to MIUI V13.0.1.0.SJEMIXM
    From: cannong-user 12 SP1A.210812.016 V13.0.1.0.SJEMIXM release-keys
    
    Drop shims and blob fixups.
    
    Change-Id: I74b59f35a6a4b35529ca6029fc50e9125b127384
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- extract-files.sh
+++ extract-files.sh
@@ -53,25 +53,6 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
-function blob_fixup() {
-    case "${1}" in
-        vendor/lib64/libudf.so)
-            "${PATCHELF}" --replace-needed "libunwindstack.so" "libunwindstack-v30.so" "${2}"
-            ;;
-        vendor/lib64/libmtkcam_stdutils.so)
-            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v30.so" "${2}"
-            ;;
-        vendor/lib/hw/audio.primary.mt6853.so)
-            ;&
-        vendor/lib64/hw/audio.primary.mt6853.so)
-            "${PATCHELF}" --replace-needed "libmedia_helper.so" "libmedia_helper-v30.so" "${2}"
-            ;;
-        lib/libmtk_vt_service.so)
-           "${PATCHELF}" --add-needed "libshim_vtservice.so" "${2}"
-           ;;
-    esac
-}
-
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
